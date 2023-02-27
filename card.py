e9325078870c724cb9ef384579481024aa237553
DJKramer314 <djkramer314@gmail.com>
2023-02-27 11:59:20 -0600

    Changed 'Your card' to 'This card'

--- card.py
+++ card.py
@@ -1,7 +1,7 @@
 class Card:
 
     def __str__(self):
-        return f"Your card is the {self.value} of {self.suit}"
+        return f"This card is the {self.value} of {self.suit}"
 
     def __init__(self, value: int, suit: str):
         self.value = value
