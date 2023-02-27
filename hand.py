498e3d4f50ab1fd518dcbfc5a89baa2639a71d5c
DJKramer314 <djkramer314@gmail.com>
2023-02-27 14:21:48 -0600

    removed the 'Hand' class and its other implimentations

--- hand.py
+++
@@ -1,6 +0,0 @@
-from card import Card
-
-
-class Hand:
-    def __init__(self, player_cards: tuple(Card)):
-        self.player_cards = player_cards
--- pokergame.py
+++ pokergame.py
@@ -1,11 +1,10 @@
 from card import Card
-from hand import Hand
 
 
 class PokerGame:
     def __init__(self,
-                 hand: Hand,
-                 table: tuple(Card) = ()):
+                 hand: tuple[Card],
+                 table: tuple[Card] = ()):
         self.hand = hand
         # Table is an optional input, as the table may not have any cards
         self.table = table
