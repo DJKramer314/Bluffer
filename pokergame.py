cab1b79f6409c4c565e0c7fd01e8463e449d2127
DJKramer314 <djkramer314@gmail.com>
2023-02-27 13:05:20 -0600

    Updated the pokergame class to use tuples and include the cards on the table

--- pokergame.py
+++ pokergame.py
@@ -1,6 +1,12 @@
+from card import Card
 from hand import Hand
 
 
 class PokerGame:
-    def __init__(self, hand: Hand):
+
+    def __init__(self,
+                 hand: Hand,
+                 table: tuple(Card) = ()):
         self.hand = hand
+        # Table is an optional input, as the table may not have any cards
+        self.table = table
