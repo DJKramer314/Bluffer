b7848b36e52e5dfc6d1b4d008d0449aaf149aa81
DJKramer314 <djkramer314@gmail.com>
2023-02-27 15:42:21 -0600

    Enhanced comments in pokergame and deleted some testing code in the calculate function

--- pokergame.py
+++ pokergame.py
@@ -2,15 +2,12 @@ from card import Card
 
 
 class PokerGame:
+    # Table is initialized as an optional input because the table may not have any cards, in which it will be initialized as an empty tuple
     def __init__(self,
                  hand: tuple[Card],
                  table: tuple[Card] = ()):
         self.hand = hand
-        # Table is an optional input, as the table may not have any cards
         self.table = table
 
     def calculate_winning_odds(self) -> float:
-        if len(self.table) == 5:
-            return 0.0
-        else:
-            return 1.0
+        return 1.0
