f4e6d22ac18e44f2b5d188a4f0d63043e1a64426
DJKramer314 <djkramer314@gmail.com>
2023-02-27 21:28:39 -0600

    Implemented the card class in a more efficient way and upgraded the string method of Card

--- card.py
+++ card.py
@@ -1,37 +1,63 @@
 class Card:
 
     def __str__(self):
-        return f"The {self.value} of {self.suit}"
+        # This variable is either a number (2-10) or a full name of a card, such as 'King'
+        name: str
 
-    # None is specified here to allow for an optional construcor argument
+        # This conditional is for special names for the face cards and Ace
+        if self.value > 10:
+            name_values = {
+                "11": "Jack",
+                "12": "Queen",
+                "13": "King",
+                "14": "Ace"
+            }
+            name = name_values[str(self.value)]
+        else:
+            name = str(self.value)
+        return f"The {name} of {self.suit}"
+
+    # None is specified here to allow for an optional constructor argument
     def __init__(self, string: str, value: int = None):
 
         if value:
             # This only occurs if a card string and value is specified in the constructor
             # While a string may also include a value, it is not known until the else statement
-            # This method, while slightly hard to read, is the only way that I can overload the constructor
+            # This process, while slightly hard to read, is the only way that I can overload the constructor
             self.value = value
             self.suit = string
 
         else:
             # This spot is only reached when a string and not a value is specified in the constructor
             # The string must be in the format "AB" where A = 2-10 or J Q K, and B = s, c, h, or d for the suit
-            suit_string = ""
+            formatted_input = self.format_input(string)
+            self.value, self.suit = formatted_input[0], formatted_input[1]
 
-            # This check is necessary to ensure that a 10 is not mistaken for a 1
-            # It is not more efficient to use negative indexing because we would still have to check if there are 3 characters
-            if string[0:2] == "10":
-                self.value = 10
-                suit_string = string[2]
-            else:
-                self.value = string[0]
-                suit_string = string[1]
+    def format_input(self, input: str) -> tuple:
+        value: int = input[0:-1]  # A number (2-10) or a letter (A, K, Q, J)
+        suit: str = input[-1]  # A letter (s, c, h, d)
 
-            suit_values = {
-                "s": "Spades",
-                "c": "Clubs",
-                "h": "Hearts",
-                "d": "Diamonds"
-            }
+        value_dictionary = {
+            "2": 2,
+            "3": 3,
+            "4": 4,
+            "5": 5,
+            "6": 6,
+            "7": 7,
+            "8": 8,
+            "9": 9,
+            "10": 10,
+            "J": 11,
+            "Q": 12,
+            "K": 13,
+            "A": 14
+        }
 
-            self.suit = suit_values[suit_string]
+        suit_dictionary = {
+            "s": "Spades",
+            "c": "Clubs",
+            "h": "Hearts",
+            "d": "Diamonds"
+        }
+
+        return (value_dictionary[value], suit_dictionary[suit])
