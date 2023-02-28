22c6444c5b9bf9ee5a30cdc203e5139f28425b6d
DJKramer314 <djkramer314@gmail.com>
2023-02-27 23:45:41 -0600

    Implemented a basic user input for the user's hand

--- main.py
+++ main.py
@@ -2,11 +2,28 @@ from pokergame import PokerGame
 from card import Card
 
 
-table = (Card("Hearts", 5),
-         Card("Hearts", 5),
-         Card("Hearts", 5),
-         Card("Hearts", 5))
+def get_user_hand():
+    user_string = input("Enter your hand: ")
+    card_strings = user_string.split(" ")
+    if len(card_strings) == 2:
+        pass
+    else:
+        print(
+            f"Wrong number of inputs. Expected 2, but received {len(card_strings)}.")
+        # Repeat through recursion
+        return get_user_hand()
 
-hand = (Card("Hearts", 5), Card("Hearts", 5))
+    try:
+        card_one = Card(card_strings[0])
+        card_two = Card(card_strings[1])
+    except:
+        print("Invalid input(s). Please try again")
+        # Repeat through recursion
+        return get_user_hand()
 
-game = PokerGame(hand, table)
+    return (card_one, card_two)
+
+
+hand = get_user_hand()
+for card in hand:
+    print(card)
