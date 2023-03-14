f4c31c87cb5d93d8099073361e6a32c625cea35f
DJKramer314 <djkramer314@gmail.com>
2023-03-14 00:37:53 -0700

    Renamed unit_test_generator for pytest clarity and added functionality for royal flush and straight flush test generation

---
+++ unit_test_generator.py
@@ -0,0 +1,36 @@
+# format:
+# def test1():
+#     hand = (Card("2s"), Card("3h"))
+#     table = ()
+#     game = PokerGame(hand, table)
+#     assert game.determine_hand() == 1
+
+def generate_test(test_name: str, hand_strings: list[str, str], table_strings: list[str], expected_output: int):
+    f = open("generate_test_output.txt", "a")
+    f.write(f"def {test_name}():\n")
+    f.write(
+        f"    hand = (Card(\"{hand_strings[0]}\"), Card(\"{hand_strings[1]}\"))\n")
+    table_string = ""
+    for string in table_strings:
+        table_string += f"Card(\"{string}\"), "
+    f.write(
+        f"    table = ({table_string})\n")
+    f.write("    game = PokerGame(hand, table)\n")
+    f.write(f"    assert game.determine_hand() == {expected_output}\n\n")
+
+
+suits = ("s", "d", "c", "h")
+values = ("2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A")
+
+# Royal Flush
+royal_flush_card_values = values[-5::]
+for suit in suits:
+    generate_test(f"test_royal_flush_{suit}", [royal_flush_card_values[0]+suit, royal_flush_card_values[1]+suit], [
+                  royal_flush_card_values[2]+suit, royal_flush_card_values[3]+suit, royal_flush_card_values[4]+suit], 10)
+
+# Straight Flush
+for value in range(0, 7):
+    straight_flush_card_values = values[value:value+5]
+    for suit in suits:
+        generate_test(f"test_straight_flush_{suit}_{value+2}_to_{value+6}", [straight_flush_card_values[0]+suit, straight_flush_card_values[1]+suit], [
+                      straight_flush_card_values[2]+suit, straight_flush_card_values[3]+suit, straight_flush_card_values[4]+suit], 9)
