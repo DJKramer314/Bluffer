2092042e6a300ad250b08a59fd523e63a6be5487
DJKramer314 <djkramer314@gmail.com>
2023-03-14 00:12:53 -0700

    Allowed the test generator to output to a txt file. This allows for larger test generation

--- test_generator.py
+++ test_generator.py
@@ -6,16 +6,14 @@
 #     assert game.determine_hand() == 1
 
 def generate_test(test_name: str, hand_strings: list[str, str], table_strings: list[str], expected_output: int):
-    print(f"def {test_name}():")
-    print(
-        f"    hand = (Card(\"{hand_strings[0]}\"), Card(\"{hand_strings[1]}\"))")
+    f = open("generate_test_output.txt", "a")
+    f.write(f"def {test_name}():\n")
+    f.write(
+        f"    hand = (Card(\"{hand_strings[0]}\"), Card(\"{hand_strings[1]}\"))\n")
     table_string = ""
     for string in table_strings:
         table_string += f"Card(\"{string}\"), "
-    print(
-        f"    table = ({table_string})")
-    print("    game = PokerGame(hand, table)")
-    print(f"    assert game.determine_hand() == {expected_output}")
-
-
-generate_test("test1", ["2s", "2s"], ["2h", "As"], 2)
+    f.write(
+        f"    table = ({table_string})\n")
+    f.write("    game = PokerGame(hand, table)\n")
+    f.write(f"    assert game.determine_hand() == {expected_output}\n")
